95ad9e19c639f80b232a38e92e29e5211b4d46df
Striver <96542456+T3Tm@users.noreply.github.com>
2023-11-14 18:13:23 +0900

    transition (#4)
    * Update jihun
    
    * display Transition

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,7 +3,8 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class Main extends JFrame {
+public class Main {
+    private static JFrame frame;
     static void init(JPanel t){
         t.setLayout(null);//패널 자유롭게 셋팅하기
         t.setBackground(new Color(115,52,211));
@@ -38,6 +39,9 @@ public class Main extends JFrame {
         t.setOpaque(true);
         return t;
     }
+    protected static JFrame getFrame() {
+    	return frame;
+    }
     static public JPanel createPlayerPanel(){
         JPanel t = new JPanel(new GridLayout(0,1,0,5));//0을 입력하면 제한없이 받는 거임.
         //플레이어는 아래로 계속 뜨게끔 만들 거임.
@@ -58,11 +62,12 @@ public class Main extends JFrame {
 
 
     Main(){
-        setSize(1280,720);
-        add(new GameRoom());
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+    	  frame = new JFrame("DreamOut");
+        frame.setSize(1280,720);
+        frame.add(new GameRoom());
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 
     public static void main(String[] args) {
