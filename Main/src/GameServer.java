d90f1f0f32cbc5a17c2b1d632df0217d5661dc03
Striver <chl020729@gmail.com>
2023-11-14 22:17:55 +0900

    server connect

--- Main/src/GameServer.java
+++ Main/src/GameServer.java
@@ -1,7 +1,9 @@
-package Main.src;
 import javax.swing.*;
-import java.io.IOException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Date;
 /*
 *  1. 현재 접속 인원 알려주기
@@ -12,17 +14,39 @@ import java.util.Date;
 public class GameServer extends JFrame {
     private ServerSocket SS;//ServerSocket;
     private int Port;
-    GameServer(int port) {
-        try{
-            SS=new ServerSocket(this.Port);
-        }catch(IOException ignored){
-
-        }
+    GameServer(int port) {//현재 프레임에 서버 시작 버튼 만들기
+        JButton b_start = new JButton("서버 시작하기");
+        b_start.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                //마우스 클릭 됐을 때 서버 시작
+                StartServer();
+            }
+        });
 
         this.Port = port;
         Date date = new Date();
         System.out.println(date);
     }
+
+    public void StartServer() {
+        try{
+            SS=new ServerSocket(this.Port);
+            System.out.println("서버가 시작되었습니다.");
+        }catch(IOException ignored){}
+        while(true){
+            try {
+                Socket s = SS.accept();
+                OutputStream out = s.getOutputStream();//해당 소켓으로 입력들 뿌려줘야 됨.
+                BufferedWriter o = (O utputStvreamWriter)out;
+            } catch (IOException e) {
+                System.out.println("비이상적으로 서버가 시작되지 않았습니다.");
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int port = 54321;
         new GameServer(port);
