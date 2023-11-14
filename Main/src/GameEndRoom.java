bc525f1f8f2575a229f369b9b9a4450c789f2d96
Striver <chl020729@gmail.com>
2023-11-14 13:12:29 +0900

    Squashed commit of the following:
    commit f2fec2e79a7e9a0e7775c2041d90099acdefa477
    Author: Striver <chl020729@gmail.com>
    Date:   Tue Nov 14 13:05:21 2023 +0900
    
        Update jihun
    
    commit 1bd5e9466bf796106ffb283a787c7201a5d2ecbe
    Author: Striver <chl020729@gmail.com>
    Date:   Mon Nov 13 19:28:04 2023 +0900
    
        Update java

---
+++ Main/src/GameEndRoom.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameEndRoom extends JPanel {
+    private JScrollPane playerScroll;//플레이어 스크롤
+    private JScrollPane displayScroll;//그림 스크롤
+    private JTextArea t_display;//그림 그린 것들이 뜨는 곳
+
+    GameEndRoom(){
+            Main.init(this);
+
+
+            //스크롤
+            playerScroll = new JScrollPane(Main.createPlayerPanel(),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            playerScroll.setSize(300,400);
+            playerScroll.setLocation(250, 160);
+
+
+
+            //사진들
+            t_display = new JTextArea();
+            t_display.setEnabled(false);
+            //스크린 스크롤
+            displayScroll = new JScrollPane(t_display,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            displayScroll.setLocation(550,160);
+            displayScroll.setSize(660,400);
+            displayScroll.setBackground(Color.WHITE);
+
+            //temp
+
+            JLabel temp= new JLabel("test");
+            temp.setSize(150,75);
+            temp.setLocation(1020,450);
+            temp.setOpaque(true);
+            temp.setBackground(Color.cyan);
+
+            add(temp);
+            add(playerScroll);
+            add(displayScroll);
+
+    }
+
+
+}
---
+++ Main/src/GameStartRoom.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameStartRoom extends JPanel {
+    private JLabel timeBar;//시간 진행률
+    private JTextArea t_display;//그림 그리는 판
+    private JTextField t_input;//글자를 입력할 수 있고 없고.
+    private JLabel l_pallete;//팔렛트
+    private JLabel client;
+
+    final int WIDTH = 900;
+
+    GameStartRoom(){
+        Main.init(this);
+        //진행바
+        timeBar = new JLabel("");
+        timeBar.setOpaque(true);
+        timeBar.setBackground(Color.WHITE);
+        timeBar.setLocation(210,40);
+        timeBar.setSize(WIDTH,40);
+
+
+        //display
+        t_display = new JTextArea();
+        t_display.setLocation(210,110);
+        t_display.setSize(WIDTH,450);
+        t_display.setEnabled(false);
+
+
+        //input
+        t_input = new JTextField();
+        //t_input.setEnabled(false);
+        t_input.setSize(WIDTH,55);
+        t_input.setLocation(210,580);
+        t_input.setHorizontalAlignment(JTextField.RIGHT);
+        t_input.setFont(new Font("맑은 고딕",Font.BOLD,23));
+
+        //L_pallete
+        l_pallete = new JLabel("");
+        l_pallete.setLocation(1150,110);
+        l_pallete.setSize(100,450);
+        l_pallete.setOpaque(true);
+        l_pallete.setBackground(Color.white);
+
+
+
+        //client
+
+        client = new JLabel("Round : 1/8");
+        client.setFont(new Font("Courier", Font.BOLD,11));
+        client.setOpaque(true);
+        client.setBackground(Color.lightGray);
+        client.setLocation(1030,110);
+        client.setSize(80,50);
+
+
+        add(timeBar);
+        add(client);
+        add(t_display);
+        add(t_input);
+        add(l_pallete);
+
+    }
+}
