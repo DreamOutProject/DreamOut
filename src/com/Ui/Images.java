2ed1ca2a512653a0d2949c4b0e4bec8f4c452fc6
yubyunggyu <yubyunggyu@naver.com>
2023-12-05 19:06:30 +0900

    images

---
+++ src/com/Ui/Images.java
@@ -0,0 +1,8 @@
+package com.Ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Images {
+    public static final Image BackImage = new ImageIcon("src/image/background.jpeg").getImage();
+}
