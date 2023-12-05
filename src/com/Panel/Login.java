708e1405ebde0b4413d4f8568de66cf0c0463c6c
yubyunggyu <yubyunggyu@naver.com>
2023-12-05 19:00:13 +0900

    login images rootPanel Update

--- src/com/Panel/Login.java
+++ src/com/Panel/Login.java
@@ -1,6 +1,7 @@
 package com.Panel;
 
 import com.Main.Main;
+import com.Ui.Colors;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,26 +12,52 @@ public class Login extends RootPanel{
     public JButton register;
     public JTextField id;
     public JTextField pw;
+
+
     public Login(Main main){
         //회원 가입 시
         this.main = main;
 
+        JPanel title = title();
         JPanel leftSide = leftSide();
         JPanel rightSide = rightSide();
 
+        add(title);
         add(leftSide);
         add(rightSide);
 
         new LoginLogic(main,login,register,id,pw);
     }
+
+    public JPanel title(){
+            JPanel t = new JPanel();
+            t.setBounds(300,50,600,200);
+            t.setBackground(Colors.BackColor);
+            Font font = new Font("바탕",Font.ITALIC,100);
+            JLabel name = new JLabel("Dream Out");
+            name.setForeground(Colors.ForeColor);
+            name.setFont(font);
+            t.add(name);
+            return t;
+    }
+
     public JPanel leftSide(){
         JPanel t = new JPanel(new GridLayout(0,2));
-        t.setBounds(200,265,220,100);
+        t.setBounds(200,365,220,100);
+        t.setBackground(Colors.BackColor);
         JLabel ids = new JLabel("ID : ");
         JLabel pws = new JLabel("PW : ");
 
+        Font id_font = new Font("바탕",Font.ITALIC,20);
+        Font pw_font = new Font("바탕",Font.ITALIC,20);
+
         id = new JTextField();
         pw = new JTextField();
+
+        ids.setFont(id_font);
+        pws.setFont(pw_font);
+        ids.setForeground(Colors.ForeColor);
+        pws.setForeground(Colors.ForeColor);
 
         t.add(ids);
         t.add(id);
@@ -40,7 +67,7 @@ public class Login extends RootPanel{
     }
     public JPanel rightSide(){
         JPanel t = new JPanel(new GridLayout(0,1));
-        t.setBounds(850,232,220,100);
+        t.setBounds(850,365,220,100);
         register = new JButton("회원가입");
         login = new JButton("로그인");
 
--- src/com/Panel/RootPanel.java
+++ src/com/Panel/RootPanel.java
@@ -2,18 +2,28 @@ package com.Panel;
 
 import com.Main.Main;
 import com.Ui.Colors;
+import com.Ui.Images;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public abstract class RootPanel extends JPanel {//모든 패널들이 가져야 하는 것들
+
     public RootPanel(){
         setLayout(null);//레이블 널이어야한다.
         setSize(Main.WIDTH,Main.HEIGHT);//크기
-        setBackground(Colors.BackColor);//배경 색깔
+        setOpaque(true);
+//        setBackground(Colors.BackColor);//배경 색깔
+        repaint();
         testing();
     }
+    public void paint(Graphics g){
+        g.drawImage(Images.BackImage,0,0,null);
+    }
+
+
 
     public void testing(){
         addMouseListener(new MouseAdapter() {
